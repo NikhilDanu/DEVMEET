07b0f95b25dc8d8fa764e673259c4ec8b1b4a9a5
NikhilDanu <ns2297877@gmail.com>
2026-04-14 14:53:50 +0530

    Delete routes directory

--- routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-
-const {signup , login} = require("../controllers/user.controller");
-
-routes.post("/signup" , signup);
-routes.post("/login", login);
-
-module.exports = routes;
